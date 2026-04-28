8600c00334d1e22121f4d875433b86037a3354bc
fdezau <f.dezau0191@gmail.com>
2026-04-27 22:57:35 -0500

    feat: CORS para Vercel

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://192.168.18.78:3000'],
+    origin: ['http://localhost:3000', 'http://192.168.18.78:3000', 'https://menuahora.vercel.app', /\.vercel\.app$/],
     credentials: true,
   });
 
